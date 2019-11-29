3b7e87a02ac6a5659f5d42acaa83832028480a38
VasilmHub <48993358+VasilmHub@users.noreply.github.com>
2019-11-30 00:42:58 +0200

    Here I will upload second main
    Check modifiers.

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,22 +11,47 @@ public class Main {
         Method[] declaredMethods = reflection.getDeclaredMethods();
         List<Method> setters = new ArrayList<>();
         List<Method> getters = new ArrayList<>();
+        List<Field> fields=new ArrayList<>();
         for (Method declaredMethod : declaredMethods) {
-            if ((!retTypeVoid(declaredMethod)) && startWith("get", declaredMethod)) {
-                getters.add(declaredMethod);
-
-            } else if (retTypeVoid(declaredMethod) && startWith("set", declaredMethod)) {
-                setters.add(declaredMethod);
-
+            int modifiers = declaredMethod.getModifiers();
+            if(isItGetter(declaredMethod)){
+                if(!Modifier.isPublic(modifiers)) {
+                    getters.add(declaredMethod);
+                }
+            }else if(isItSetter(declaredMethod)){
+                if(!Modifier.isPrivate(modifiers)) {
+                    setters.add(declaredMethod);
+                }
+            }
+        }
+        Field[] declaredFields = reflection.getDeclaredFields();
+        for (Field field : declaredFields) {
+            if(!Modifier.isPrivate(field.getModifiers())){
+                fields.add(field);
             }
         }
 
-        print(getters, setters);
+        print(getters, setters,fields);
     }
 
-    private static void print(List<Method> getters, List<Method> setters) {
-        getters.stream().sorted((a,b)-> a.getName().compareTo(b.getName())).forEach(declaredMethod -> System.out.println(declaredMethod.getName() + " will return class " + declaredMethod.getReturnType().getName()));
-        setters.stream().sorted((a,b)-> a.getName().compareTo(b.getName())).forEach(declaredMethod -> System.out.println(declaredMethod.getName() + " and will set field of class " + declaredMethod.getParameterTypes()[0].getName()));
+    private static boolean isItSetter(Method declaredMethod) {
+        if (retTypeVoid(declaredMethod) && startWith("set", declaredMethod)) {
+           return true;
+        }
+        return false;
+    }
+
+    private static boolean isItGetter(Method declaredMethod) {
+        if ((!retTypeVoid(declaredMethod)) && startWith("get", declaredMethod)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static void print(List<Method> getters, List<Method> setters, List<Field> fields) {
+        fields.stream().sorted((a,b)->a.getName().compareTo(b.getName())).forEach(a-> System.out.println(a.getName()+" must be private!"));
+        getters.stream().sorted((a,b)-> a.getName().compareTo(b.getName())).forEach(declaredMethod -> System.out.println(declaredMethod.getName() + " have to be public!" ));
+        setters.stream().sorted((a,b)-> a.getName().compareTo(b.getName())).forEach(declaredMethod -> System.out.println(declaredMethod.getName() + " have to be private!"));
     }
 
     private static boolean retTypeVoid(Method declaredMethod) {
